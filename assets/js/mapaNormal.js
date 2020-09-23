39fcddd5c41dd573906eab04944244f2f13d8647
Hnerique <henri633@gmail.com>
2020-09-23 11:58:48 -0300

    Pagina teste - validação de assunto e zoom no mapa - 23/09/2020

---
+++ assets/js/mapaNormal.js
@@ -0,0 +1,4 @@
+function mapaNormal() {
+    mapa.style.width = "800px";
+    mapa.style.height = "600px";
+}
--- assets/js/script.js
+++ assets/js/script.js
@@ -9,6 +9,10 @@
 let nome = window.document.getElementById("nome")
 let email = document.querySelector("#email")
 let assunto = document.querySelector("#assunto")
+let nomeOk = false
+let emailOk = false
+let assuntoOk = false
+let mapa = document.querySelector("#mapa")
 
 nome.style.width = '100%'
 email.style.width = '100%'
@@ -19,8 +23,9 @@ function validaNome() {
         txtNome.innerHTML = "Nome Invalido!"
         txNome.style.color = "red"
     }else{
-        txt.innerHTML = "Nome Valido!"
-        txt.style.color = "green"
+        txtNome.innerHTML = "Nome Valido!"
+        txtNo.style.color = "green"
+        nomeOk = true
     }
 }
 function validaEmail(){
@@ -32,5 +37,33 @@ function validaEmail(){
     }else{
         txtEmail.innerHTML = "Email Valido!"
         txtEmail.style.color = "green"
+        emailOk = true
     }
+}
+function validaAssunto(){
+    let txtAssunto = document.querySelector("#txtAssunto")
+
+    if(assunto.value.length >= 2){
+        txtAssunto.innerHTML = "Texto é muito grande, digite no máximo 250 caracteres."
+        txtAssunto.style.color = "red"
+        txtAssunto.style.display = "block"
+    }else{
+        txtAssunto.style.display = "none"
+        assuntoOk = true
+    }
+}
+function enviar(){
+    if(nomeOk == true && emailOk == true && assuntoOk == true){
+        alert("Formulario enviado com sucesso!")
+    }else{
+        alert("Preencha o formulario corretamente!")
+    }
+}
+function mapaZoom(){
+    mapa.style.width = "800px"
+    mapa.style.height = "600px"
+}
+function mapaNormal(){
+    mapa.style.width = "400px"
+    mapa.style.height = "250px"
 }
